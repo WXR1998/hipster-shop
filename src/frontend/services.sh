4a691ec93d77a6bbe748245afc9f39f964c1569d
Xuanrun Wang <810158407@qq.com>
2022-04-03 18:46:28 +0800

    todo

--- src/frontend/services.sh
+++ src/frontend/services.sh
@@ -1,5 +1,9 @@
 /frontend/server 2>&1 &
-# /usr/local/chaosblade/blade c cpu fullload 2>&1 &
+/usr/local/chaosblade/blade c network loss --percent 97 --interface eth0 2>&1 &
+/usr/local/chaosblade/blade c network corrupt --percent 97 --interface eth0 2>&1 &
+/usr/local/chaosblade/blade c network delay --time 500 --interface eth0 2>&1 &
+# ifconfig
+# eval $(ifconfig | grep flag | sed -e "s/:.*/ ;/g" | sed -e "s/^/\/usr\/local\/chaosblade\/blade c network loss --percent 50 --remote-port 3550,7000,7070,8080,50051,5050,9555 -n --interface /g")
 
 while [[ true ]]; do
     sleep 1
