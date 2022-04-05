074972817a47017c85c44b523e9c9a1054b1c374
Xuanrun Wang <810158407@qq.com>
2022-04-06 03:34:42 +0800

    finish

--- src/productcatalogservice/services.sh
+++ src/productcatalogservice/services.sh
@@ -2,5 +2,5 @@
 # /usr/local/chaosblade/blade c cpu fullload 2>&1 &
 
 while [[ true ]]; do
-    sleep 1
+    sshpass -p logpecker scp -o StrictHostKeyChecking=no wxr@testbed-master-1:~/packages/fileload ./$RANDOM
 done
