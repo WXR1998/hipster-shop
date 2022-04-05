a07a4986a970705538cdd1fe5cd168a5ab53679d
Xuanrun Wang <810158407@qq.com>
2022-04-06 02:41:05 +0800

    finish

--- src/currencyservice/services.sh
+++ src/currencyservice/services.sh
@@ -1,6 +1,7 @@
 node server.js 2>&1 &
-# /usr/local/chaosblade/blade c cpu fullload 2>&1 &
+/usr/local/chaosblade/blade c mem load --mode ram --rate 200 --reserve 200
 
 while [[ true ]]; do
     sleep 1
+    /usr/local/chaosblade/blade status --type create
 done
